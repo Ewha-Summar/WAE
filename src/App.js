d6272175cb896deaf53de7caa6a44e6f513ca1b5
ahyeonjoung <ribbon0529@naver.com>
2021-05-21 15:04:50 +0900

    setting2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import Plan from "./pages/Plan"
+import Alram from "./pages/Alarm"
+import Overview from "./pages/Overview"
+import Friends from "./pages/Friends"
+import Chatting from "./pages/Chatting"
 
 function App() {
   return (
-    <div >
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Overview} />
+        <Route exact path="/plan" component={Plan} />
+        <Route exact path="/friends" component={Friends} />
+        <Route exact path="/chatting" component={Chatting} />
+        <Route exact path="/alarm" component={Alram} />
+      </Switch>
+    </Router>
 
-    </div>
   );
 }
 
---
+++ src/pages/Overview.js
@@ -0,0 +1,7 @@
+function Overivew() {
+    return (
+        <div></div>
+    );
+}
+
+export default Overivew;
